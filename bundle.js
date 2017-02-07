18f29342816c047660148b79710f88185dfa9b93
wangytangy <yue.bruce.wang@gmail.com>
2017-02-07 13:04:27 -0500

    Add two input fields

--- bundle.js
+++ bundle.js
@@ -44,18 +44,52 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	let airports = __webpack_require__(2);
+	let airports = __webpack_require__(1);
+	let id;
 
-	function fetchAirports() {
-	  console.log(airports.length);
-	};
+	function handleInputChange(e) {
+	  //loop through airports, filter by input
+	  //create <option> element, set it's value
+	  //append option to <datalist>
+	  clearTimeout(id);
+	  id = setTimeout(function() {
 
-	fetchAirports();
+	    let input = e.target.value.toLowerCase();
+	    console.log(input);
+	    let dataList = document.getElementById('airports');
+	    while (dataList.firstChild) dataList.removeChild(dataList.firstChild);
+
+	    let filteredList = airports.filter((port) => {
+	      return (port.IATA.toLowerCase().indexOf(input) !== -1 ||
+	      port.city.toLowerCase().indexOf(input) !== -1 ||
+	      port.name.toLowerCase().indexOf(input) !== -1)
+	    }).slice(0, 10);
+
+	    filteredList.forEach((item) => {
+	      let option = document.createElement('option');
+	      option.value = `${item.IATA}` + " " + `${item.name}` + " - " + `${item.city}`;
+	      dataList.appendChild(option);
+	    });
+
+	    console.log(filteredList);
+
+	  }, 200);
+	}
+
+	function attachInputListeners() {
+	  let elements = document.getElementsByTagName('input');
+	  for (let i = 0; i < elements.length; i++) {
+	    elements[i].addEventListener("keyup", handleInputChange);
+	  }
+	}
+
+	document.addEventListener("DOMContentLoaded", function() {
+	  attachInputListeners();
+	});
 
 
 /***/ },
-/* 1 */,
-/* 2 */
+/* 1 */
 /***/ function(module, exports) {
 
 	module.exports = [
